be653e3e1498de7ec6b99f42812dc80f0b42712a
zKunene <kunene88@icloud.com>
2026-03-07 17:22:02 +0200

    Builder pattern

--- src/main/java/com/example/financial/common/event/TransactionReceivedEvent.java
+++ src/main/java/com/example/financial/common/event/TransactionReceivedEvent.java
@@ -3,16 +3,25 @@ package com.example.financial.common.event;
 import lombok.Data;
 import com.example.financial.common.type.TransactionType;
 import com.example.financial.common.type.TransactionStatus;
+import lombok.Builder;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
 @Data
+@Builder
 public class TransactionReceivedEvent {
+
+    @NotNull(message = "TransactionId cannot be null")
     private UUID transactionId;
+
     private String accountId;
+
+    @NotNull(message = "Transaction amount cannot be null")
     private BigDecimal amount;
+    
     private TransactionType type;
     private TransactionStatus status;
     private Instant timestamp;
